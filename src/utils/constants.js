3751c2a11ce2e3c5057f86705811e813d96bfa8d
sibuspd <sibuspd@gmail.com>
2024-12-08 07:48:04 +0530

    Changed the constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const product_api_url = "https://dummyjson.com/products";
+export const product_api_url = "http://localhost:3000/products";
+export const cart_api_url = "http://localhost:3000/cart";
+export const auth_api_url = "http://localhost:3000/auth";
